c80e376daab97615c969ce7cf92f4ec8aa38fc79
Onursoyar <soyaronur@gmail.com>
2022-05-10 09:02:29 +0000

    Add and edit the quiz questions for javascript page

---
+++ assets/javascript/quizquestions.js
@@ -0,0 +1,403 @@
+const quizMe1 = [{
+        question: "Which planet is the hottest",
+        text: "Venus",
+        text: "Mars",
+        text: "Saturn",
+        text: "Mercury",
+
+        correctAnswer: "Venus"
+    },
+    {
+        question: "In Pirates of the Caribbean, what was Captain Jack Sparrow's ship's name?",
+
+        text: "The Marauder",
+        text: "The Black Pearl",
+        text: "The Black Python",
+        text: "The Slytherin",
+
+        correctAnswer: "The Black Pearl"
+    },
+    {
+        question: "According to Forrest Gump, “life was like…”",
+
+        text: "A bag of lemons",
+        text: "A handful of roses",
+        text: "A lollipop",
+        text: "A box of chocolates",
+
+        correctAnswer: "A box of chocolates"
+    },
+    {
+        question: "What is the rarest blood type?",
+
+        text: "O",
+        text: "A",
+        text: "B",
+        text: "AB-Negative",
+
+        correctAnswer: "AB-Negative",
+    },
+    {
+        question: "How many bones are there in the human body?",
+
+        text: "206",
+        text: "205",
+        text: "201",
+        text: "209",
+
+        correctAnswer: "206",
+    },
+    {
+        question: "Fe is the chemical symbol for…",
+
+        text: " Zinc",
+        text: "Hydrogen",
+        text: "Fluorine",
+        text: "Iron",
+
+        correctAnswer: "Iron",
+    },
+    {
+        question: "What language is the most spoken worldwide?",
+
+        text: "Chinese",
+        text: "Spanish",
+        text: "Arabic",
+        text: "English",
+
+        correctAnswer: "Chinese",
+    },
+    {
+        question: "What is Shakespeare's shortest tragedy?",
+
+        text: "Macbeth",
+        text: "Hamlet",
+        text: "Romeo & Juliet",
+        text: "Othello",
+
+        correctAnswer: "Macbeth",
+    },
+    {
+        question: "Which planet in our solar system is the largest?",
+
+        text: "Jupiter",
+        text: "Saturn",
+        text: "Neptune",
+        text: "Earth",
+
+        correctAnswer: "Jupiter",
+    },
+    {
+        question: "Which ocean is the largest?",
+
+        text: "Indian",
+        text: "Pacific",
+        text: "Atlantic",
+        text: "Arctic",
+
+        correctAnswer: "Pacific",
+    }
+]
+
+const quizMe2 = [{
+        question: "Which New York City building is the tallest:",
+        text: "Empire State Building",
+        text: "Bank of America Tower",
+        text: "One World Trade Center",
+        text: "Statue of Liberty",
+
+        correctAnswer: "One World Trade Center"
+    },
+    {
+        question: "Which country gifted the Statue of Liberty to the U.S.?",
+
+        text: "Germany",
+        text: "China",
+        text: "France",
+        text: "Italy",
+
+        correctAnswer: "France"
+    },
+    {
+        question: "Who painted the Mona Lisa?",
+
+        text: "Van Gogh",
+        text: "da Vinci",
+        text: "Picasso",
+        text: "Monet",
+
+        correctAnswer: "da Vinci"
+    },
+    {
+        question: "In which city were Anne Frank and her family in hiding?",
+
+        text: "Paris",
+        text: "Amsterdam",
+        text: "Brussels",
+        text: "Frankfurt",
+
+        correctAnswer: "Amsterdam"
+    },
+    {
+        question: "Which country consumes the most chocolate?",
+
+        text: "Spain",
+        text: "Germany",
+        text: "North America",
+        text: "Switzerland",
+
+        correctAnswer: "Switzerland"
+    },
+    {
+        question: "What is Sodium Chloride?",
+
+        text: "Salt",
+        text: "Sugar",
+        text: "Chlorine",
+        text: "Bleach",
+
+        correctAnswer: "Salt"
+    },
+    {
+        question: "In the Bible, how does the Virgin Mary learn of her pregnancy with baby Jesus?",
+
+        text: "God tells her",
+        text: "the angel Gabriel tells her",
+        text: "she has a dream about it",
+        text: "a doctor tells her",
+
+        correctAnswer: "the angel Gabriel tells her"
+    },
+    {
+        question: "Who wrote the songs for The Lion King?",
+
+        text: "Elton John",
+        text: "Phil Collins",
+        text: "Celine Dion",
+        text: "Stevie Wonder",
+
+        correctAnswer: "Elton John"
+    },
+    {
+        question: "In the U.S. version of The Office, Michael Scott burns his foot on:",
+
+        text: "hot water",
+        text: "pavement/cement",
+        text: "rocks on fire",
+        text: "a George Foreman Grill",
+
+        correctAnswer: "a George Foreman Grill"
+    },
+    {
+        question: "What is the longest river in the world?",
+
+        text: "Amazon",
+        text: "Congo",
+        text: "Nile",
+        text: "Hudson",
+
+        correctAnswer: "Nile"
+    },
+
+]
+const quizMe3 = [{
+        question: "How many degrees are in a circle?",
+        text: "360",
+        text: "180",
+        text: "150",
+        text: "359",
+
+        correctAnswer: "360"
+    },
+    {
+        question: "Which city is known as the City of Love?",
+
+        text: "Rome",
+        text: "Barcelona",
+        text: "Paris",
+        text: "New York City",
+
+        correctAnswer: "Paris"
+    },
+    {
+        question: "As an adult, how many teeth should you have in your mouth?",
+
+        text: "35",
+        text: "32",
+        text: "30",
+        text: "42",
+
+        correctAnswer: "32"
+    },
+    {
+        question: "What was the name of the boy who won Willy Wonka’s factory?",
+
+        text: "Charlie Baxter",
+        text: "Charlie Brown",
+        text: "Charlie Bones",
+        text: "Charlie Bucket",
+
+        correctAnswer: "Charlie Bucket"
+    },
+    {
+        question: "What is the complementary color of green?",
+
+        text: "Blue",
+        text: "Yellow",
+        text: "Red",
+        text: "Purple",
+
+        correctAnswer: "Red"
+    },
+    {
+        question: "Han Solo is a character from which movie series:",
+
+        text: "Harry Potter",
+        text: "Star Wars",
+        text: "Lord of the Rings",
+        text: "Indiana Jones",
+
+        correctAnswer: "Star Wars"
+    },
+    {
+        question: "How many Harry Potter books are there?",
+
+        text: "7",
+        text: "8",
+        text: "6",
+        text: "10",
+
+        correctAnswer: "7"
+    },
+    {
+        question: "The UK is made up of the following countries: England, Ireland, Wales, and…",
+
+        text: "France",
+        text: "Hungary",
+        text: "Scotland",
+        text: "Austria",
+
+        correctAnswer: "Scotland"
+    },
+    {
+        question: "How many elements are there on the periodic table?",
+
+        text: "112",
+        text: "118",
+        text: "120",
+        text: "143",
+
+        correctAnswer: "118"
+    },
+    {
+        question: "Where is the United Nations Headquarters?",
+
+        text: "D.C.",
+        text: "NYC",
+        text: "Philadelphia",
+        text: "Orlando",
+
+        correctAnswer: "NYC"
+    },
+]
+
+const quizMe4 = [{
+        question: "In what year did women get the right to vote?",
+        text: "1910",
+        text: "1920",
+        text: "1930",
+        text: "1940",
+
+        correctAnswer: "1920"
+    },
+    {
+        question: "Neil Armstrong was the first man…",
+
+        text: "on Mars",
+        text: "on a spacecraft alone",
+        text: "on the Moon",
+        text: "to travel to the sun",
+
+        correctAnswer: "on the Moon"
+    },
+    {
+        question: "What does “FBI” stand for?",
+
+        text: "Female Body Inspector",
+        text: "Federal Bureau of Investigation",
+        text: "Federal Business of Investigation",
+        text: "Federal Bureau of Inspection",
+
+        correctAnswer: "Federal Bureau of Investigation"
+    },
+    {
+        question: "How often does the moon orbit the Earth?",
+
+        text: "every 7 days",
+        text: "every 27 days",
+        text: "every 30 days",
+        text: "every 365 days",
+
+        correctAnswer: "every 27 days"
+    },
+    {
+        question: "In Greek Mythology, who is the Queen of the Underworld?",
+
+        text: "Pandora",
+        text: "Medusa",
+        text: "Helen",
+        text: "Persephone",
+
+        correctAnswer: "Persephone"
+    },
+    {
+        question: "Who founded Microsoft?",
+
+        text: "Bill Hader",
+        text: "Steve Jobs",
+        text: "Bill Gates",
+        text: "Mark Zuckerberg",
+
+        correctAnswer: "Bill Gates"
+    },
+    {
+        question: "Steve Jobs is known for wearing a black…",
+
+        text: "button-down shirt",
+        text: "t-shirt",
+        text: "turtleneck",
+        text: "blazer",
+
+        correctAnswer: "Turtleneck"
+    },
+    {
+        question: "Which two planets in our solar system are known as “ice giants”?",
+
+        text: "Neptune and Jupiter",
+        text: "Uranus and Pluto",
+        text: "Pluto and Jupiter",
+        text: "Neptune and Uranus",
+
+        correctAnswer: "Neptune and Uranus"
+    },
+    {
+        question: "How many eyes does a spider have?",
+
+        text: "8",
+        text: "9",
+        text: "2",
+        text: "10",
+
+        correctAnswer: "8"
+    },
+    {
+        question: "What is the name of the company that published the Mario Kart video game?",
+
+        text: "Nintendo",
+        text: "Electronic Arts (EA)",
+        text: "SEGA",
+        text: "Xbox",
+
+        correctAnswer: "Nintendo"
+    },
+];
